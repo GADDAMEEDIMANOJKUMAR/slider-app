1ef6c24d86ca67cd10c6a46971285d0cc9e5f8ba
manoj <manojgoud335@gmail.com>
2025-07-31 01:38:24 +0530

    time change

--- src/slide.jsx
+++ src/slide.jsx
@@ -11,11 +11,9 @@
 //         setIndex((prevIndex) => (prevIndex - 1 + props.length) % props.length);
 //     };
 
-
 //   return (
 //     <div className='slide'>
 //         <span className='icon' onClick={handlePrevious}><i class="bi bi-arrow-left-circle-fill"></i></span>
-
 
 //      <div className='card'>
 //             <div className='img-container' key ={id}>
@@ -24,7 +22,7 @@
 //                 <p className='title'>{title}</p>
 //                 <p className='para'>{quote}</p>
 //             </div>
-//         </div>  
+//         </div>
 //             <span className='icon' onClick={handleNext}><i class="bi bi-arrow-right-circle-fill"></i></span>
 //     </div>
 //   )
@@ -32,8 +30,8 @@
 
 // export default slide
 
-import  { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 const Slide = ({ people }) => {
   const [index, setIndex] = useState(0);
@@ -50,29 +48,29 @@ const Slide = ({ people }) => {
   useEffect(() => {
     const clear = setTimeout(() => {
       handleNext();
-    }, 8000);
+    }, 10000);
 
     // Automatically change slide every 3 seconds
-}, [index]);
-  
+  }, [index]);
+
   const { id, name, title, quote, image } = people[index];
 
   return (
-    <div className='slide'>
-      <span className='icon' onClick={handlePrevious}>
+    <div className="slide">
+      <span className="icon" onClick={handlePrevious}>
         <i className="bi bi-arrow-left-circle-fill"></i>
       </span>
 
-      <div className='card' key={id}>
-        <div className='img-container'>
-          <img src={image} alt={name} className='img' />
-          <h2 className='head'>{name}</h2>
-          <p className='title'>{title}</p>
-          <p className='para'>{quote}</p>
+      <div className="card" key={id}>
+        <div className="img-container">
+          <img src={image} alt={name} className="img" />
+          <h2 className="head">{name}</h2>
+          <p className="title">{title}</p>
+          <p className="para">{quote}</p>
         </div>
       </div>
 
-      <span className='icon' onClick={handleNext}>
+      <span className="icon" onClick={handleNext}>
         <i className="bi bi-arrow-right-circle-fill"></i>
       </span>
     </div>
